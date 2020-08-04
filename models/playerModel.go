cae379e790b550b5b985f29b892e5141df9a820e
kovansky <root@f4dev.me>
2020-08-04 22:26:28 +0200

    Created PlayerModel constructor

--- models/playerModel.go
+++ models/playerModel.go
@@ -4,3 +4,7 @@ type PlayerModel struct {
 	PlayerName  string
 	PlayerLevel int
 }
+
+func NewPlayerModel(playerName string, playerLevel int) *PlayerModel {
+	return &PlayerModel{PlayerName: playerName, PlayerLevel: playerLevel}
+}
