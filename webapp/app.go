72aaa0361e14de587a4acdfe19e073d97e63a4f0
kovansky <root@f4dev.me>
2020-08-06 19:01:04 +0200

    Webapp runner

---
+++ webapp/app.go
@@ -0,0 +1,20 @@
+package webapp
+
+import (
+	"github.com/kovansky/dndEncounterCalculator/misc"
+	"net/http"
+)
+
+func App() {
+	mux := http.NewServeMux()
+	mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./webapp/public/"))))
+	mux.HandleFunc("/", party)
+
+	server := &http.Server{
+		Addr:    "127.0.0.1:12346",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	misc.Check(err)
+}
