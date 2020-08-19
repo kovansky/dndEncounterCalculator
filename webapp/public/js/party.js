2cea33bfb5eb5704b726fbfca8531ec8f767c2aa
kovansky <root@f4dev.me>
2020-08-20 01:18:07 +0200

    Collect from inputs

--- webapp/public/js/party.js
+++ webapp/public/js/party.js
@@ -1,15 +1,35 @@
 $(document).ready(function() {
-    cloneTemplate('#charInputTmpl', '#content')
+    cloneTemplate("#charInputTmpl", "#content")
 
-    $('.charNew').click(() => {
-        cloneTemplate('#charInputTmpl', '#content')
+    $(".charNew").click(() => {
+        cloneTemplate("#charInputTmpl", "#content")
     })
 
-    $('#content').on('click', '.charDelete', function() {
-        let count = $('.tmpl').length
+    $("#content").on("click", ".charDelete", function() {
+        let count = $(".tmpl").length
 
         if(count > 2) {
-            $(this).closest('.tmpl').remove()
+            $(this).closest(".tmpl").remove()
         }
+    })
+
+    $(".nextStep").click(function() {
+        let characters = []
+
+        $("#content").find(".charInputContainer").each(function() {
+            let char = {
+                name: null,
+                level: null
+            }
+
+            char.name = $(this).children()[0].value
+            char.level = $(this).children()[1].value
+
+            characters.push(char)
+        })
+
+        console.log(characters)
+
+        // ToDo: invoke go function
     })
 })
