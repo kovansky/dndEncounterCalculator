493952f56a453d80da65f1c88ca67a00433015de
kovansky <root@f4dev.me>
2020-09-20 22:35:29 +0200

    Added documentation in controllers directory

--- controllers/mainController.go
+++ controllers/mainController.go
@@ -8,35 +8,76 @@ import (
 	"github.com/webview/webview"
 )
 
+//enemies is a variable that holds all monsters added in the monsters form in the Main View.
 var enemies *models.EnemiesModel
 
+//MainWindow is a controller function of Main View. It loads a view into existing WebView window
 func MainWindow(wv webview.WebView) {
+	// Adjust window data to view
 	wv.SetTitle("D&D Encounter Calculator") // language
 	wv.SetSize(1000, 675, webview.HintFixed)
 
-	jsonParty, err := json.Marshal(Party)
+	// On view open loads previous state (monsters list), if existed.
+	err := wv.Bind("loadWindowState", func() string {
+		// Check, if enemies value was set. If it wasn't, this is the first run (there is no state to be restored)
+		if enemies != nil {
+			var monstersAsArray []models.MonsterModel
+
+			// Convert monsters from map to slice
+			for _, monster := range enemies.GroupMonsters {
+				monstersAsArray = append(monstersAsArray, monster)
+			}
+
+			// Marshal monsters array into json
+			jsonE, err := json.Marshal(monstersAsArray)
+			misc.Check(err)
+
+			stringed := string(jsonE)
+
+			return stringed
+		} else {
+			return ""
+		}
+	})
 	misc.Check(err)
 
+	// Returns Party data as json
 	err = wv.Bind("getPartyData", func() string {
+		// Marshal Party data to json
+		jsonParty, err := json.Marshal(Party)
+		misc.Check(err)
+
 		return string(jsonParty)
 	})
 	misc.Check(err)
 
+	// Calculates encounter difficulty and XP values
 	err = wv.Bind("calculateResults", func(monstersString json.RawMessage) string {
 		if enemies == nil {
 			enemies = models.NewEnemiesModel()
 		}
 
 		var (
-			monsters   []models.MonsterModel
-			modifier   enum.EncounterModifier
+			// Slice of monsters
+			monsters []models.MonsterModel
+
+			// Difficulty modifier, depends on monsters amount and party size
+			modifier enum.EncounterModifier
+
+			// XP value after applying difficulty modifier
 			adjustedXP float32
+
+			// Encounter difficulty
 			difficulty enum.EncounterDifficulty
-			results    models.ResultsModel
+
+			// Results of calculation
+			results models.ResultsModel
 		)
 
+		// Load monsters list from view into monsters slice
 		json.Unmarshal(monstersString, &monsters)
 
+		// Add monsters to enemies variable
 		for _, monster := range monsters {
 			monster.Update()
 			enemies.AddMonster(monster)
@@ -63,26 +104,7 @@ func MainWindow(wv webview.WebView) {
 	})
 	misc.Check(err)
 
-	err = wv.Bind("loadWindowState", func() string {
-		if enemies != nil {
-			var monstersAsArray []models.MonsterModel
-
-			for _, monster := range enemies.GroupMonsters {
-				monstersAsArray = append(monstersAsArray, monster)
-			}
-
-			jsonE, err := json.Marshal(monstersAsArray)
-			misc.Check(err)
-
-			stringed := string(jsonE)
-
-			return stringed
-		} else {
-			return ""
-		}
-	})
-	misc.Check(err)
-
+	// Navigates window to Party View
 	err = wv.Bind("editParty", func() bool {
 		PartyWindow(wv)
 
@@ -90,5 +112,6 @@ func MainWindow(wv webview.WebView) {
 	})
 	misc.Check(err)
 
+	// Opens Main View in window
 	wv.Navigate("http://127.0.0.1:12354/main")
 }
--- controllers/updateController.go
+++ controllers/updateController.go
@@ -10,42 +10,52 @@ import (
 	"github.com/webview/webview"
 )
 
+//UpdateWindow is a controller function of Update View (dialog). It creates a WebView window
 func UpdateWindow(currentVersion models.AppVersionModel, remoteVersion models.AppVersionModel) {
+	// Create webview window, and defer destroying it
 	uw := webview.New(false)
 	defer uw.Destroy()
 
+	// Adjust window data to view
 	uw.SetTitle("Update avaliable!") // language
 	uw.SetSize(600, 200, webview.HintFixed)
 
+	// On view open passes data from backend to view
 	err := uw.Bind("loadWindowState", func() string {
+		// Holds data to return
 		var data = make(map[string]interface{})
 
+		// Adds versions information to return
 		data["current"] = currentVersion
 		data["remote"] = remoteVersion
 
+		// Marshal return data to json
 		jsonData, err := json.Marshal(data)
 		misc.Check(err)
 
-		stringed := string(jsonData)
-
-		return stringed
+		return string(jsonData)
 	})
 	misc.Check(err)
 
+	// Dialog controls (buttons) logic
 	err = uw.Bind("retValue", func(code int) int {
+		// If "yes" ("download update") button clicked, open update URL in browser
 		if code == 1 {
 			url := fmt.Sprintf(constants.APP_UPDATE_URL, remoteVersion.ToString())
 
 			browser.OpenURL(url)
 		}
 
+		// Close dialog window, regardless of button clicked
 		uw.Terminate()
 
 		return code
 	})
 	misc.Check(err)
 
+	// Opens Update View in window
 	uw.Navigate("http://127.0.0.1:12354/update")
 
+	// Runs window code
 	uw.Run()
 }
