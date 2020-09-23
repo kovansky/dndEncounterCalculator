cfa8ecad928dec210abed7b6c0b302d7de7c7398
kovansky <root@f4dev.me>
2020-09-23 19:59:39 +0200

    Added documentation in main.go file

--- main.go
+++ main.go
@@ -8,11 +8,17 @@ import (
 	"github.com/webview/webview"
 )
 
+//main is the heart of application, the runner of the rest of the program
 func main() {
+	// Run webapp in separate goroutine
 	go webapp.App()
+	// Check for updates in separate goroutine
 	go func() {
+		// Get current application version
 		appVersion := models.GetAppVersion()
+		// Check for updates from remote
 		isUpdate, rMajor, rMinor, rPatch, rChannel := appVersion.CheckForUpdates()
+		// Create AppVersionModel from remote data
 		remoteAvm := models.AppVersionModel{
 			Major:   rMajor,
 			Minor:   rMinor,
@@ -20,18 +26,23 @@ func main() {
 			Channel: rChannel,
 		}
 
+		// If update avaliable, open Update Dialog
 		if isUpdate {
 			controllers.UpdateWindow(*appVersion, remoteAvm)
 		}
 	}()
 
+	// Create new webview instance and defer destroying it
 	wv := webview.New(true)
 	defer wv.Destroy()
 
+	// Bind runError function for JS to be avaliable in all views using this webview instance
 	err := wv.Bind("runError", misc.ThrowError)
 	misc.Check(err)
 
+	// Run first window - party window
 	controllers.PartyWindow(wv)
 
+	// Runs window code
 	wv.Run()
 }
